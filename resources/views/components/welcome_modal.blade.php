d4da02983e4ce68db2d5986b235dfefd60494c8f
simon-brd <brochard.simon85140@gmail.com>
2019-12-04 23:26:24 +0100

    info modal

--- resources/views/components/welcome_modal.blade.php
+++ resources/views/components/welcome_modal.blade.php
@@ -2,20 +2,20 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Infos sur le fonctionnement</h4>
+                <h4 class="modal-title">Comment Reflex'Yon fonctionne ?</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p>Cette application suit une étude sur l'apprentissage par répétitions.<br>
-                    Chaque quizz que vous allez réaliser devra être validé 4 fois.<br>
-                    Pour validé votre quizz, il faudra atteindre une note minimale.<br>
-                    Quizz validé <i class="far fa-grin-stars" style="margin-right: 7px;color:#27ae60"></i><br>
-                    Quizz bientôt à refaire <i class="far fa-meh-rolling-eyes" style="margin-right: 7px;color:#d35400"></i><br>
-                    Quizz a refaire <i class="far fa-sad-tear" style="margin-right: 7px; color:#e74c3c" ></i><br>
-                    L'écart de temps entre chaque répétition augmentera après chaque validation.<br>
-                    Vous pouvez aussi ajouter des amis afin de comparer vos résultats sur les différents quizz.</p>
+                <h5>Cette application suit une étude sur l'apprentissage par répétitions.</h5>
+                    <h5>Chaque quizz que vous allez réaliser devra être validé 4 fois.</h5>
+                    <h5>Pour validé votre quizz, il faudra atteindre une note minimale.</h5>
+                    <h5>Quizz validé <i class="far fa-grin-stars" style="margin-right: 7px;color:#27ae60"></i></h5>
+                    <h5>Quizz bientôt à refaire <i class="far fa-meh-rolling-eyes" style="margin-right: 7px;color:#d35400"></i></h5>
+                    <h5>Quizz a refaire <i class="far fa-sad-tear" style="margin-right: 7px; color:#e74c3c" ></i></h5>
+                    <h5>L'écart de temps entre chaque répétition augmentera après chaque validation.</h5>
+                    <h5>Vous pouvez aussi ajouter des amis afin de comparer vos résultats sur les différents quizz.</h5>
             </div>
         </div>
         <!-- /.modal-content -->
